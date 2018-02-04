2375721b8fad2f0a13c3066c5b13552b101ff253
LampIsMe <34934612+LampIsMe@users.noreply.github.com>
2018-02-03 20:18:59 -0800

    Update Armor.java

--- Armor.java
+++ Armor.java
@@ -1,30 +1,18 @@
-public class Armor{
-    int armorx;
-    int armory;
-    int durability;
-    public Armor(int armornum){
-        armorx = (int)(Math.random() * 15) ;
-        armory = (int)(Math.random() * 15);
+public class Armor extends Items{
+    private String type;
+    private int durability;
+    public Armor(String type){
+        this.type = type;
+        setItem(type);
         durability = (int)(Math.random() * 40) + 20;
     }
-
-    public int getx(){
-        return armorx;
-    }
-
-    public int gety(){
-        return armory;
+    public String getName(){
+        return type;
     }
 
     public int getDefence(){
         return durability;
     }
 
-    public void resetx(){
-        armorx = (int)(Math.random() * 10) ;
-    }
-    
-    public void resety(){
-        armory = (int)(Math.random() * 10) ;
-    }
+
 }
