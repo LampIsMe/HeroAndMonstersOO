b7b1ade90babee37e2a23a241d81d5385ef6b853
ethanwu6 <ethanwu6@gmail.com>
2018-02-04 12:50:22 -0800

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -2,10 +2,20 @@ public class Potion{
     int potionx;
     int potiony;
     int potency;
-    public Potion(int potionnum){
+    String type;
+    public Potion(String inittype){
         potionx = (int)(Math.random() * 15);
         potiony = (int)(Math.random() * 15);
-        potency = (int)(Math.random() * 30) + 5;
+        type = inittype;
+        if (type.equals("Minor Healing Potion" )){
+            potency = (int)(Math.random()*30) + 1;
+        }
+        if (type.equals("Healing Potion")){
+            potency = (int)(Math.random()*40) + 30;
+        }
+        if (type.equals("Major Healing Potion")){
+            potency = (int)(Math.random()*40) + 50;
+        }
     }
 
     public int getx(){
@@ -16,15 +26,24 @@ public class Potion{
         return potionx;
     }
 
-    public int Potency(){
+    public int getPotency(){
         return potency;
     }
 
     public void resetx(){
         potiony = (int)(Math.random() * 10) ;
     }
-    
+
     public void resety(){
         potionx = (int)(Math.random() * 10);
     }
+
+    public String toString2(){
+        return "You have gained a " + type + "which heals " + getPotency();
+    }
+
+    public String toString(){
+        return type;
+    }
+
 }
