63cf72bab4f87902cd75dec067d1f48c24d943be
LampIsMe <34934612+LampIsMe@users.noreply.github.com>
2018-02-03 20:18:22 -0800

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,38 +1,37 @@
 import java.util.*;
-public class Hero{
+public class Hero extends Items{
     private int health;
-    private int daggerdamage;
-    private int armor;
+    Sword weapon;
+    Armor armor;
     private int herox;
     private int heroy;
     private int damage;
-    Sword weapon = new Sword(1, "dagger");
+    private int speed;
     public Hero(){
         health = 100;
         heroy = 0;
         herox = 14;
+        weapon = new Sword("dagger");
+        armor = new Armor("none");
         
-        armor = 0;
+        speed = 25;
     }
 
     public void addHealth(int add){
         health = add + health;
     }
-    public void setType(String t){
-        weapon.setType(t);
+    
+    public int getspeed(){
+        return speed;
+    }
+    
+    public void setspeed(int a){
+        speed = speed + a;
     }
     
     
-    public int getequip(){
-        return weapon.getDamage();
-    }
-    
-    public void addDefence(int add){
-        armor = add + armor;
-    }
-    
-    public int getDefence(){
-        return armor;
+    public double getDefence(){
+        return armor.getDefence();
     }
     
     public void up(){
@@ -54,6 +53,8 @@ public class Hero{
     public boolean checkup(){
         if (herox -1<0){
             System.out.println("You moved out of bounds");
+            try{Thread.sleep(1000);}catch(InterruptedException e){System.out.println("got interrupted!");
+                }
             return false;
         }
         return true;
@@ -62,6 +63,8 @@ public class Hero{
     public boolean checkleft(){
         if (heroy -1<0){
             System.out.println("You moved out of bounds");
+            try{Thread.sleep(1000);}catch(InterruptedException e){System.out.println("got interrupted!");
+                }
             return false;
         }
         return true;
@@ -70,6 +73,8 @@ public class Hero{
     public boolean checkright(){
         if (heroy +1>14){
             System.out.println("You moved out of bounds");
+            try{Thread.sleep(1000);}catch(InterruptedException e){System.out.println("got interrupted!");
+                }
             return false;
         }
         return true;
@@ -78,6 +83,8 @@ public class Hero{
     public boolean checkdown(){
         if (herox +1>14){
             System.out.println("You moved out of bounds");
+            try{Thread.sleep(1000);}catch(InterruptedException e){System.out.println("got interrupted!");
+                }
             return false;
         }
         return true;
@@ -94,9 +101,9 @@ public class Hero{
     public int gethealth(){
         return health;
     }
-    
+   
     public int herodamage(){
-        return daggerdamage;
+        return weapon.getDamage();
     }
     
     public void updateHealth(int herohealth){
