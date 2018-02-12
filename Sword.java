d254af62d55570fcd3bebe1f4786274cec2d72a7
LampIsMe <34934612+LampIsMe@users.noreply.github.com>
2018-02-11 21:25:12 -0800

    Update Sword.java

--- Sword.java
+++ Sword.java
@@ -1,25 +1,11 @@
 public class Sword{
     int damage;
     String type;
+    
     public Sword(String swordtype){
         type = swordtype;
-    }
-    public void setType(String t){
-        type = t;
-        
-    }
-    public String getName(){
-        return type;
-        
-    }
-    
-    public int getDamage(){
-        if (type.equals("dagger")){
-            damage = (int)(Math.random()*30) + 1;
-            
-        }else if (type.equals("short sword")){
-            damage = (int)(Math.random()*20) + 30;
-            
+        if (type.equals("short sword")){
+            damage = (int)(Math.random()*30) + 30;
             
         }else if (type.equals("long sword")){
             damage = (int)(Math.random()*20) + 50;
@@ -28,7 +14,19 @@ public class Sword{
         }else if (type.equals("battle axe (OP)")){
             damage = (int)(Math.random()*100) + 100;
         }
+    }
+    public void setType(String t){
+        type = t;
+        
+    }
+    public int getDamage(){
         return damage;
     }
+    public String getName(){
+        return type;
+        
+    }
+    public String toString(){
+        return type;
+    }
 }
-
